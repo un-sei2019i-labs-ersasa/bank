fca6e8b934503afb251feaac6a5246ebaf9866ef
samoralespu <samoralespu@unal.edu.co>
2019-06-03 14:17:29 -0500

    cambios

---
+++ mobile-app/bank-app/testFinal/app/src/main/java/com/example/testfinal/dataAccess/models/Transaction.java
@@ -0,0 +1,4 @@
+package com.example.testfinal.dataAccess.models;
+
+public class Transaction {
+}
---
+++ mobile-app/bank-app/testFinal/app/src/main/java/com/example/testfinal/presentation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.testfinal.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.testfinal.R;
+import com.example.testfinal.dataAccess.database.Database;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ingresar;
+    Database helper= new Database(this,"Base", null, 1);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ingresar=(Button)findViewById(R.id.btn_ingresar);
+        ingresar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                EditText identificacion=(EditText)findViewById(R.id.id_txt);
+                EditText password=(EditText)findViewById(R.id.pass_txt);
+                if(identificacion.getText().equals("") || password.getText().equals("")) {
+                    Toast.makeText(getApplicationContext(), "CAMPO VACIO", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    boolean cursor = helper.validaInformacion(Integer.valueOf(identificacion.getText().toString()), Integer.valueOf(password.getText().toString()));
+                    if (cursor == true) {
+                        Toast.makeText(getApplicationContext(), "SATISFACTORIO", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "USUARIO O CONTRASENIA INCORRECTOS", Toast.LENGTH_LONG).show();
+                    }
+                    identificacion.setText("");
+                    password.setText("");
+                }}});
+    }
+}
